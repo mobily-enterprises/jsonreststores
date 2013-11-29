fd5c92899a5997e5da3e80d6afc90dd2a3dba334
Tony Mobily <tonymobily@gmail.com>
2013-11-30 07:13:29 +0800

    Fixed tingo test, which is also the default one now so that tests can be run without Mongodb running

--- test-tingo.js
+++ test-tingo.js
@@ -3,7 +3,7 @@ var
 
 , declare = require('simpledeclare')
 
-, J = require('./jsonreststores')
+, J = require('./JsonRestStores.js')
 , SimpleDbLayer = require('simpledblayer')
 , Db = require('tingodb')().Db
 , TingoMixin = require('simpledblayer-tingo')
