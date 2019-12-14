222a977dbd61ef3335984f2d67ce64bf7ae52997
Tony Mobily <tonymobily@gmail.com>
2019-12-15 07:30:45 +0800

    Comment tidy up

--- jsonreststores.js
+++ jsonreststores.js
@@ -8,12 +8,6 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-/*
-NOTE. When creating a store, you can take the following shortcuts:
-  * Don't specify `paramIds`. If not specified, it will be worked out from publicURL
-  * Don't specify `idProperty`. If idProperty not specified, it will be assumed last element of paramIds
-*/
-
 const e = require('allhttperrors')
 const path = require('path')
 const fs = require('fs')
