95dbac2a1f15f7581af765931b937f0d378a1c82
Tony Mobily <tonymobily@gmail.com>
2021-03-01 09:39:12 +0800

    New architecture, delegating more things to JsonRestStores

--- jsonreststores.js
+++ jsonreststores.js
@@ -17,6 +17,11 @@ const registryByName = {}
 const registryByVersion = {}
 
 const Store = exports = module.exports = class {
+  // Hooks to inject code in workflow
+  async beforeValidate (request) { }
+  async validate (request) { return [] }
+  async checkPermissions (request) { return { granted: true } }
+
   // ****************************************************
   // *** ATTRIBUTES THAT CAN TO BE DEFINED IN PROTOTYPE
   // ****************************************************
@@ -113,23 +118,273 @@ const Store = exports = module.exports = class {
   // They need to satisfy the JsonRestStores DB API
 
   async implementFetch (request) {
-    throw (new Error('implementFetch not implemented, store is not functional'))
+    request.method = request.method || 'get'
+    request.inMethod = 'implementFetch'
+    request.options = request.options || {}
+
+    const id = request.params[this.idProperty]
+    if (!id) throw new Error('request.params needs to contain idProperty for implementFetch')
+
+    // Checking of permission must be delegated to the implementing function which
+    // must call this.implementFetchPermissions(request) once request.record is set
   }
 
+  async implementFetchPermissions (request) {
+    // Check for permissions
+    const { granted, message } = await this.checkPermissions(request)
+    if (!granted) throw new this.constructor.ForbiddenError(message)
+  }
+
+  _enrichBodyWithParamIds (request) {
+    // SIDE_EFFECT: body[paramId]
+    this.paramIds.forEach((paramId) => {
+      if (typeof (request.params[paramId]) !== 'undefined') {
+        request.body[paramId] = request.params[paramId]
+      }
+    })
+  }
+
+  // Check that paramsId are actually legal IDs using
+  // paramsSchema.
+  async _validateParams (request, skipIdProperty) {
+    const fieldErrors = []
+
+    const params = request.params || {}
+
+    // Params is empty: nothing to do, optimise a little
+    if (params.length === 0) return
+
+    // Check that _all_ paramIds are in params
+    // (Direct requests don't use URL, so check)
+    this.paramIds.forEach((k) => {
+      // "continue" if id property is to be skipped
+      if (skipIdProperty && k === this.idProperty) return
+
+      // Required paramId not there: puke!
+      if (typeof (params[k]) === 'undefined') {
+        fieldErrors.push({ field: k, message: 'Field required in the URL/param: ' + k })
+      }
+    })
+    // If one of the key fields was missing, puke back
+    if (fieldErrors.length) throw new this.constructor.BadRequestError({ errors: fieldErrors })
+
+    // Prepare skipFields, depending on skipIdProperty
+    const skipFields = []
+    if (skipIdProperty) {
+      skipFields.push(this.idProperty)
+    }
+
+    // Validate params
+    const { validatedObject, errors } = await this.schema.validate(params, { onlyObjectValues: true, skipFields })
+    if (errors.length) throw new this.constructor.BadRequestError({ errors: errors })
+
+    return validatedObject
+  }
+
+  // SIDE_EFFECT: record
+  cleanup (record) {
+    const r = Object.assign({}, record)
+    for (const k in r) {
+      if (typeof this.schema.structure[k] === 'undefined') delete r[k]
+    }
+    return r
+  }
+
+  // SIDE_EFFECTS:
+  //   request.body[beforeIdField] moved to request.beforeId
+  //   request.body (enriched with paramIds)
+  //   request.originalBody
+  //   request.params (whole object replaced by _validateParams)
+  //   request.originalParams (whole object replaced by _validateParams)
   async implementInsert (request) {
-    throw (new Error('implementInsert not implemented, store is not functional'))
+    request.method = request.method || 'post'
+    request.inMethod = 'implementInsert'
+    request.options = request.options || {}
+
+    if (this.positionField) {
+      request.beforeId = request.body[this.beforeIdField]
+      delete request.body[this.beforeIdField]
+    }
+
+    // validateParam
+    request.originalParams = request.params || {}
+    request.params = await this._validateParams(request, true)
+
+    // Add paramIds to body
+    this._enrichBodyWithParamIds(request)
+
+    // Assigning an empty record, since there is no data
+    request.record = {}
+
+    // Prepare the errors array
+    let errors = []
+
+    // This is an important hook as developers might want to
+    // manipulate request.body before validation (e.g. non-schema custom fields)
+    // or manipulate the request itself
+    await this.beforeValidate(request, errors)
+
+    const fullRecord = this.fullRecordOnInsert || request.options.fullRecordOnInsert
+
+    const emptyAsNull = typeof request.options.emptyAsNull !== 'undefined'
+      ? !!request.options.emptyAsNull
+      : this.emptyAsNull
+
+    const canBeNull = typeof request.options.canBeNull !== 'undefined'
+      ? !!request.options.canBeNull
+      : this.canBeNull
+
+    // Validate input. This is light validation.
+    const { validatedObject, errors: validationErrors } = await this.schema.validate(request.body, {
+      emptyAsNull,
+      canBeNull,
+      onlyObjectValues: !fullRecord
+    })
+
+    request.originalBody = request.body
+    request.body = validatedObject
+
+    // Enrich the error array with the extra validation errors (if any)
+    if (validationErrors.length) errors = [...errors, ...validationErrors]
+
+    // Check for permissions
+    const { granted, message } = await this.checkPermissions(request, errors)
+    if (!granted) throw new this.constructor.ForbiddenError(message)
+
+    // Call the validate hook. This will carry more expensive validation once
+    // permissions are granted
+    await this.validate(request, errors)
+    if (errors.length) throw new this.constructor.UnprocessableEntityError({ errors })
+
+    // Reinstating request.body.beforeId must be delegated to the implementing function which
+    // must call this.restoreBeforeIdInRecord(request) in order to restore it
+  }
+  //   request.originalRecord
+
+  // SIDE_EFFECT:
+  //   request.record (created, if not already set)
+  //   request.body[beforeIdField] (maybe deleted)
+  //   request.params (whole object replaced by _validateParams())
+  //   request.body (added paramIds)
+  //   request.originalBody
+  //   request.options
+  async implementUpdate (request) {
+    request.method = request.method || 'put'
+    request.inMethod = 'implementUpdate'
+    request.options = request.options || {}
+
+    if (this.positionField) {
+      request.beforeId = request.body[this.beforeIdField]
+      delete request.body[this.beforeIdField]
+    }
+
+    // validateParam
+    request.originalParams = request.params || {}
+    request.params = await this._validateParams(request)
+
+    const id = request.params[this.idProperty]
+    if (!id) throw new Error('request.params needs to contain idProperty for implementUpdate')
+
+    // Add paramIds to body
+    this._enrichBodyWithParamIds(request)
+
+    // Load the record, if it is not yet present in request as `record`
+    if (!request.record) {
+      request.record = await this.implementFetch(request)
+    }
+
+    // Prepare the errors array
+    let errors = []
+
+    // This is an important hook as developers might want to
+    // manipulate request.body before validation (e.g. non-schema custom fields)
+    // or manipulate the request itself
+    await this.beforeValidate(request, errors)
+
+    const fullRecord = this.fullRecordOnUpdate || request.options.fullRecordOnUpdate
+
+    // Validate input. This is light validation.
+    const { validatedObject, errors: validationErrors } = await this.schema.validate(request.body, {
+      emptyAsNull: request.options.emptyAsNull || this.emptyAsNull,
+      onlyObjectValues: !fullRecord,
+      record: request.record
+    })
+
+    request.originalBody = request.body
+    request.body = validatedObject
+
+    // Enrich the error array with the extra validation errors (if any)
+    if (validationErrors.length) errors = [...errors, ...validationErrors]
+
+    // Check for permissions
+    const { granted, message } = await this.checkPermissions(request, errors)
+    if (!granted) throw new this.constructor.ForbiddenError(message)
+
+    await this.validate(request, errors)
+    if (errors.length) throw new this.constructor.UnprocessableEntityError({ errors })
+
+    // Reinstating request.body.beforeId must be delegated to the implementing function which
+    // must call this.restoreBeforeIdInRecord(request) in order to restore it
   }
 
-  async implementUpdate (request) {
-    throw (new Error('implementUpdate not implemented, store is not functional'))
+  async restoreBeforeIdInRecord (request) {
+    // Sneak beforeId back in. This will tell the client
+    // where to place the record, allowing for (maybe) repositioning
+    if (typeof request.beforeId !== 'undefined') {
+      request.record.beforeId = request.beforeId
+    }
   }
 
   async implementDelete (request) {
-    throw (new Error('implementDelete not implemented, store is not functional'))
+    request.method = request.method || 'delete'
+    request.inMethod = 'implementDelete'
+    request.options = request.options || {}
+
+    const id = request.params[this.idProperty]
+    if (!id) throw new Error('request.params needs to contain idProperty for implementDelete')
+
+    // Load the record, if it is not yet present in request as `record`
+    if (!request.record) {
+      request.record = await this.implementFetch(request) || null
+    }
+    request.body = {}
+    request.originalBody = {}
+
+    // Check for permissions
+    const { granted, message } = await this.checkPermissions(request)
+    if (!granted) throw new this.constructor.ForbiddenError(message)
   }
 
   async implementQuery (request) {
-    throw (new Error('implementQuery not implemented, store is not functional'))
+    request.method = request.method || 'getQuery'
+    request.inMethod = 'implementQuery'
+
+    // Don't allow options pollution. We can change the reference request.options
+    // because requests are one-use only. However, the options object itself must
+    // be left intact since it may not be single-use only
+    // The sort object is also copied clean if passed through `options`
+    request.options = request.options ? { ...request.options } : {}
+    const actualSort = request.options.sort || this.defaultSort
+    request.options.sort = actualSort ? { ...actualSort } : {}
+
+    // Sanitise request.skip and request.limit
+    // Limit is set in options, or the store-wide defaults defaultLimitOnQueries
+    request.options.skip = request.options.skip || 0
+    request.options.limit = request.options.limit || this.defaultLimitOnQueries
+
+    // Validate the search schema
+    const { validatedObject, errors } = await this.searchSchema.validate(request.options.conditionsHash, { onlyObjectValues: true })
+    if (errors.length) throw new this.constructor.BadRequestError({ errors: errors })
+    request.options.conditionsHash = validatedObject
+
+    // Stubs to avoid 981313 empty checks later
+    request.originalBody = {}
+    request.body = {}
+    request.record = {}
+
+    // Check for permissions
+    const { granted, message } = await this.checkPermissions(request)
+    if (!granted) throw new this.constructor.ForbiddenError(message)
   }
 
   // ****************************************************
@@ -218,7 +473,7 @@ const Store = exports = module.exports = class {
     // Check that the method is implemented
     if (!this.handlePost && request.remote) throw new Store.NotImplementedError()
 
-    return await this.implementInsert(request) || null
+    return await this.implementInsert(request) || { [this.idProperty]: request.body[this.idProperty] }
   }
 
   async _makePut (request) {
@@ -242,10 +497,10 @@ const Store = exports = module.exports = class {
 
     if (!request.record) {
       // Execute actual DB operation
-      return await this.implementInsert(request, 'put') || null
+      return await this.implementInsert(request, 'put') || { [this.idProperty]: request.params[this.idProperty] }
     } else {
       // Execute actual DB operation
-      return await this.implementUpdate(request, 'put') || null
+      return await this.implementUpdate(request, 'put') || { [this.idProperty]: request.params[this.idProperty] }
     }
   }
 
@@ -253,12 +508,12 @@ const Store = exports = module.exports = class {
     // Check that the method is implemented
     if (!this.handleGet && request.remote) throw new Store.NotImplementedError()
 
-    const record = await this.implementFetch(request) || null
+    const record = await this.implementFetch(request)
 
     // Record not there: not found error!
     if (!record) throw new Store.NotFoundError()
 
-    return record
+    return record || { [this.idProperty]: request.params[this.idProperty] }
   }
 
   async _makeGetQuery (request) {
