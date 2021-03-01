0ea01f38e7eeca6316c4e338c539bcab81739ee0
Tony Mobily <tonymobily@gmail.com>
2021-03-01 13:06:25 +0800

    Initial untested commit (never run) of memory store

---
+++ jsonreststores-mem.js
@@ -0,0 +1,145 @@
+/*
+Copyright (C) 2016 Tony Mobily
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+let data
+const firstBy = require('thenby')
+
+const Mixin = (superclass) => class extends superclass {
+  //
+  async transformResult (request, op, recordOrSet) { return null }
+
+  constructor () {
+    super()
+
+    data = []
+  }
+
+  reposition (currentPos) {
+    //
+    // No position field: exit right away
+    if (!this.beforeIdField) return
+
+    const record = data[currentPos]
+
+    const beforeId = record[this.beforeIdField]
+
+    // No beforeId defined: will leave it where it is
+    if (typeof beforeId === 'undefined') return
+
+    // The current position is last and beforeId is null: it's already where it
+    // should be (last)
+    if (currentPos === data.length - 1 && beforeId === null) return
+
+    const newPos = data.findIndex(el => el[this.idProperty] === beforeId)
+    if (newPos === -1) return
+    data.splice(newPos, 0, data.splice(currentPos, 1)[0])
+  }
+
+  // Input:
+  // - request.body
+  // Output: an object (saved record)
+
+  async implementInsert (request) {
+    await super.implementInsert(request)
+
+    request.record = request.body
+
+    data.push(request.record)
+    this.reposition(data.length - 1)
+
+    // implementUpdate() needs to have this in order to restore the
+    // previously deleted record.beforeId
+    this.restoreBeforeIdInRecord(request)
+
+    return request.record
+  }
+
+  // Input:
+  // - request.params (query)
+  // - request.body (data)
+  // Output: an object (updated record, refetched)
+  //
+  async implementUpdate (request) {
+    await super.implementUpdate(request)
+
+    const currentPos = data.findIndex(el => el[this.idProperty] === request.record[this.idProperty])
+    data[currentPos] = request.record
+    this.reposition(currentPos)
+
+    // implementUpdate() needs to have this in order to restore the
+    // previously deleted record.beforeId
+    this.restoreBeforeIdInRecord(request)
+
+    return request.record
+  }
+
+  // Input: request.params (with key this.idProperty set)
+  // Output: nothing
+  async implementDelete (request) {
+    await super.implementDelete(request)
+
+    const currentPos = data.findIndex(el => el[this.idProperty] === request.record[this.idProperty])
+    data.splice(currentPos, 1)
+  }
+
+  // Input: request.params, request.options.[conditionsHash,skip,limit,sort]
+  // Output: { data: [], grandTotal: ? }
+  async implementQuery (request) {
+    await super.implementQuery(request)
+
+    let retData = data
+
+    if (Object.keys(request.options.conditionsHash).length) retData = this.filterByConditions(request, retData)
+    if (Object.keys(request.options.sort).length) retData = this.sortBy(request, retData)
+    if (request.options.skip) retData = retData.splice(request.options.skip, 0)
+    if (request.options.limit) retData = retData.slice(0, request.options.limit)
+
+    return { data: retData, grandTotal: retData.length }
+  }
+
+  async filterByConditions (request, d) {
+    const ch = request.options.conditionsHash
+
+    return d.filter(item => {
+      for (const key in ch) {
+        if (typeof item[key] === 'undefined' || item[key] !== ch[key]) return false
+      }
+      return true
+    })
+  }
+
+  async sortBy (request, d) {
+    const sortFields = request.options.sort
+    const sortKeys = Object.keys(sortFields)
+    const sortConditions = firstBy()
+    for (let i = 1, l = sortKeys.length; i < l; i++) {
+      sortConditions.thenBy(sortFields[i], { ignoreCase: true, direction: sortFields[i] === 1 })
+    }
+    return d.sort(sortConditions)
+  }
+
+  // Input: request.params (with key this.idProperty set)
+  // Output: an object
+  async implementFetch (request) {
+    await super.implementFetch(request)
+
+    const currentPos = data.findIndex(el => el[this.idProperty] === request.record[this.idProperty])
+
+    request.record = data[currentPos]
+
+    // Requested by the API: when implementing implementFetch(), this function
+    // must be called when request.record is set
+    if (request.record) this.implementFetchPermissions(request)
+
+    return request.record
+  }
+}
+
+exports = module.exports = Mixin
